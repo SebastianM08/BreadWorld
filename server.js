185fd1f47e3b535e357c6f48c6274faaf568cbde
SebastianM08 <2puntoscreativo@gmail.com>
2023-06-23 22:05:26 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -8,13 +8,22 @@ console.log(PORT)
 
 //middleware
 const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController) 
+app.use('/breads', breadsController)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 //ROUTES
 app.get('/', (req,res)=>{
     res.send('Welcome to Breads!')
 })
 
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+  
 app.listen(PORT, ()=>{
     console.log('listening on port:', PORT)
 })
